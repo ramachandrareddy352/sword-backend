630db441dc122663d694ff2b5d02199ba14dcc82
tavanam rama <rcrtavanam@gmail.com>
2026-03-31 13:14:49 +0530

    multi langugue support

--- src/app.ts
+++ src/app.ts
@@ -1,15 +1,26 @@
 import { config } from "dotenv";
 import type { Request, Response } from "express";
 
-import server from "./config/server";
+import server, { app } from "./config/server";
 import { connectRedis } from "./config/redis";
+import { initI18n } from "./config/i18n";
+import i18nextMiddleware from "i18next-http-middleware";
 
 config();
 
 const PORT = process.env.PORT || 5000;
 
+async function startServer() {
+  const i18nInstance = await initI18n();
 
-server.listen(PORT, async () => {
-  await connectRedis();
-  console.log(`Server running on port ${PORT}`);
-});
+  app.use(i18nextMiddleware.handle(i18nInstance)); // ✅ attach BEFORE routes
+
+  const PORT = process.env.PORT || 5000;
+
+  server.listen(PORT, async () => {
+    await connectRedis();
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+startServer();
